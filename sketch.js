127825609e4a75caab7d2de76f4353b0459d7089
José Roveda Jr <contato@rovedajr.com>
2019-12-12 09:47:55 -0300

    Animation with dots
    The flowfield is filled with animated dots.

--- sketch.js
+++ sketch.js
@@ -1,29 +1,62 @@
-var inc = 0.01
+var inc = 0.1
+var scl = 10
+var cols, rows
+
+var fr
+
+var zoff = 0
+
+particles = []
+
+var flowfield
 
 function setup() {
-  createCanvas(400, 400)
+  createCanvas(300, 300)
+  cols = floor(width / scl)
+  rows = floor(height / scl)
+  fr = createP('')
+
+  flowfield = new Array(cols * rows)
+
+  for (let i = 0; i < 100; i++) {
+    particles[i] = new Particle()
+  }
+
 }
 
 function draw() {
-
+background(255)
 var yoff = 0
 
-  loadPixels()
-for (let y = 0; y < width; y++) {
+for (let y = 0; y < rows; y++) {
   var xoff = 0
-  for (let x = 0; x < width; x++) {
-    var index = (x + y * width) * 4
-    var r = noise(xoff, yoff) * 255
-    pixels[index] = r
-    pixels[index+1] = r
-    pixels[index+2] = r
-    pixels[index+3] = 255
+  for (let x = 0; x < cols; x++) {
+    var index = (x + y * cols)
+    var angle = noise(xoff, yoff, zoff) * TWO_PI * 4
+    var v = p5.Vector.fromAngle(angle)
+    v.setMag(0.5)
+    flowfield[index] = v
 
     xoff += inc
+    stroke(0,50)
+    // push()
+    // translate(x * scl, y * scl)
+    // rotate(v.heading())
+    // strokeWeight(1)
+    // line(0,0,scl,0)
+    // pop()    
     }
     yoff += inc
-
+    zoff += 0.0001
   }
-  updatePixels()
-  // noLoop()
+    for (let i = 0; i < particles.length; i++) {
+      
+      particles[i].follow(flowfield )
+      particles[i].update()
+      particles[i].show()
+      particles[i].edges()
+      
+    }
+
+  fr.html(floor(frameRate()))
 }
